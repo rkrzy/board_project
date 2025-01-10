37c32443d4707e9d8817cb589cad2b216f2caa22
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-10 20:09:09 +0900

    5장 : 게시글 읽기 Read - findById() - findAll() - 생성한 객체를 모델에 담아서 다음 페이지에 반환이 가능 - mustache는 {{}}를 이용해서 Model을 통해 받아온 데이터를 View로 출력이 가능하다.

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,8 +7,13 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -35,6 +40,21 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         return "";
+    }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
     }
 
 }
